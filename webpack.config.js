9cb42b57216d6460e4f1e98bd68e202a3b1a3628
Fatma Hussein <fartunhussein346@gmail.com>
2023-08-08 19:20:55 +0300

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,17 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
