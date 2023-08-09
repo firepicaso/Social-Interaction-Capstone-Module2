856d91732ac05824cd21a87ed974a7974e7f0aa6
Fatma Hussein <fartunhussein346@gmail.com>
2023-08-09 15:41:00 +0300

    added menu items on the homePage

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
-import _ from 'lodash';
 import './styles.css';
+
+fetch('https://www.themealdb.com/api/json/v1/1/filter.php?a=canadian')
+  .then((res) => res.json())
+  .then((data) => {
+    const mealsArray = data.meals;
+    mealsArray.forEach((meal) => {
+      const menuContainer = document.querySelector('.menu-container');
+      const menucard = document.createElement('div');
+      menucard.classList.add('menu-card');
+      const mealItem = `  <img src="${meal.strMealThumb}"/>
+      <div class="menu-title">
+          <h2>${meal.strMeal}</h2>
+          <i class="fa-regular fa-heart" style="color: #feae01;"></i>
+      </div>
+      <h3 class="like-counter"> 10 Likes</h3>
+      <button>Comment</button>`;
+      menucard.insertAdjacentHTML('beforeend', mealItem);
+      menuContainer.appendChild(menucard);
+    });
+  }).catch(
+    (error) => console.log(error),
+  );
