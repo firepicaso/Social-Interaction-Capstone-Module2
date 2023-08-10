c443d960e2f8b75c7c47b44d3915c9ff37e65c10
Mustakim <m.m.kiron@gmail.com>
2023-08-10 17:58:15 +0600

    Format modal

--- src/index.js
+++ src/index.js
@@ -5,3 +5,16 @@ import getMeals from './modules/getMeals.js';
 window.addEventListener('load', async () => {
   mealsArray(await getMeals());
 });
+
+const openModal = document.querySelector('.comment-button');
+const closeModal = document.querySelector('.fa-xmark');
+const modal = document.querySelector('.modal');
+
+openModal.addEventListener('click', () => {
+  openModal.preventDefault();
+  modal.classList.add('show');
+});
+
+closeModal.addEventListener('click', () => {
+  modal.classList.remove('show');
+});
