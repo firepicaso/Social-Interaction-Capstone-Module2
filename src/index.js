ece2730e2ab3da950540a835740ff2a506ce4883
Mustakim <m.m.kiron@gmail.com>
2023-08-11 02:18:48 +0600

    Configure modal popup

--- src/index.js
+++ src/index.js
@@ -24,3 +24,33 @@ closeModal.addEventListener('click', () => {
 });
 
 //involvement app UID: XHPfnw05OEfRfN8iPO6q
+
+document.addEventListener('click', async (event) => {
+  const openModal = event.target.closest('.comment-button');
+  
+  if (openModal) {
+    modal.classList.add('show');
+    
+    const itemId = event.target.id;
+    console.log(itemId);
+    
+    // Assuming you have a function to get menu details by itemId
+    
+    const menuDetails = async () => {
+      const res = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${itemId}`);
+      const data = await res.json();
+      return data.meals;
+    };
+
+    // Populate modal content with menu data
+    const mealImage = modal.querySelector('.meal-image');
+    const mealName = modal.querySelector('.meal-name');
+    const category = modal.querySelector('.category');
+    const videoLink = modal.querySelector('.video-link');
+
+    mealImage.src = menuDetails.strMealThumb;
+    mealName.textContent = menuDetails.strMeal;
+    category.textContent = menuDetails.strCategory;
+    videoLink.textContent = menuDetails.strYoutube;
+  }
+});
