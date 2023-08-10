884715f89b134f5fce86698a0ae644e2932bfcd8
Mustakim <m.m.kiron@gmail.com>
2023-08-11 03:09:26 +0600

    Fix modal popup

--- src/index.js
+++ src/index.js
@@ -8,49 +8,41 @@ window.addEventListener('load', async () => {
 });
 
 const modal = document.querySelector('.modal');
-const openModal = document.querySelector('.comment-button');
 const closeModal = document.querySelector('.fa-xmark');
-
-document.addEventListener('click', (event) => {
-  const openModal = event.target.closest('.comment-button');
-  
-  if (openModal) {
-    modal.classList.add('show');
-  }
-});
 
 closeModal.addEventListener('click', () => {
   modal.classList.remove('show');
 });
 
-//involvement app UID: XHPfnw05OEfRfN8iPO6q
+const menuDetails = async (id) => {
+  const res = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+  const data = await res.json();
+  console.log(data.meals[0]);
+  return data.meals[0];
+};
 
 document.addEventListener('click', async (event) => {
   const openModal = event.target.closest('.comment-button');
   
   if (openModal) {
     modal.classList.add('show');
-    
+
     const itemId = event.target.id;
     console.log(itemId);
-    
-    // Assuming you have a function to get menu details by itemId
-    
-    const menuDetails = async () => {
-      const res = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${itemId}`);
-      const data = await res.json();
-      return data.meals;
-    };
 
+    menuDetails(itemId);
+    
     // Populate modal content with menu data
-    const mealImage = modal.querySelector('.meal-image');
+    // const mealImage = modal.querySelector('.meal-image');
     const mealName = modal.querySelector('.meal-name');
     const category = modal.querySelector('.category');
     const videoLink = modal.querySelector('.video-link');
 
-    mealImage.src = menuDetails.strMealThumb;
+    // mealImage.src = menuDetails.strMealThumb;
     mealName.textContent = menuDetails.strMeal;
     category.textContent = menuDetails.strCategory;
     videoLink.textContent = menuDetails.strYoutube;
   }
 });
+
+//involvement app UID: XHPfnw05OEfRfN8iPO6q
